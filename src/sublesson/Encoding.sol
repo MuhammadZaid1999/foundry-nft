94b1367ccf77c50c8c5653bcd52743b76b673725
MuhammadZaid1999 <zaid.aptechiic@gmail.com>
2025-07-09 01:56:43 +0500

    practice

--- src/sublesson/Encoding.sol
+++ src/sublesson/Encoding.sol
@@ -35,14 +35,6 @@ contract Encoding {
         return someString;
     }
 
-    function encodeString1() public pure returns (bytes memory) {
-        return abi.encode("Hi Mom! ", "Miss you!"); 
-    }
-
-    function encodeStrings2() public pure returns (bytes memory) {
-        return abi.encode("Hi Mom!    ", "  Miss       you!      "); 
-    }
-
     function encodeNumber() public pure returns (bytes memory) {
         bytes memory number = abi.encode(1);
         return number;
@@ -64,35 +56,16 @@ contract Encoding {
         return addr;
     }
 
-    function encodeData() public pure returns (bytes memory) {
-        bytes1 num = 0x23;
-        bytes4 num1 = 0x12345678;
-        bytes memory byt = "ZAID1234565433i33i4QWEE@@@$$##";
-        string memory someStr = "some string";
-        bytes memory data = abi.encode(50, someStr, true, address(100), num, num1, byt, 0x546);
-        return data;
-    }
-
-    function encodeData2(int num, int[] memory nums) public pure returns (bytes memory) {
-        string[5] memory strData = ["1","2","3","4","5"];
-        bytes[3] memory bytesData = [bytes("0x12300041"), "0x546783453", "0x34567893"];
-        bytes2[3] memory bytesData1 = [bytes2(0x1234), 0x5123, 0x3456];
-        bytes memory data = abi.encode([1,2,3], ["1","2","3"],[0x1, 0x111, 0x4543344555], num, strData, nums, User(1, "name"), [User(1, "name"), User(2,"Name")], bytesData, bytesData1);
-        return data;
-    }
-
     // ---------- packed encoding ----------- //
     function encodePackedString() public pure returns (bytes memory) {
         bytes memory someString = abi.encodePacked("some string");
         return someString;
     }
 
-    function encodePackedString1() public pure returns (bytes memory) {
-        return abi.encodePacked("Hi Mom! ", "Miss you!"); 
-    }
-
-    function encodePackedStrings2() public pure returns (bytes memory) {
-        return abi.encodePacked("Hi Mom!    ", "  Miss       you!      "); 
+    function encodePackedNumber() public pure returns (bytes memory) {
+        uint256 someNumber = 10278645554431;
+        bytes memory number = abi.encodePacked(someNumber);
+        return number;
     }
 
     function encodePackedAddress() public pure returns (bytes memory) {
@@ -109,20 +82,6 @@ contract Encoding {
         bytes4 val = 0x54678456;
         bytes memory addr = abi.encodePacked(val);
         return addr;
-    }
-
-    function encodePackedData() public pure returns (bytes memory) {
-        bytes1 num = 0x23;
-        bytes4 num1 = 0x12345678;
-        bytes memory byt = "ZAID1234565433i33i4QWEE@@@$$##";
-        bytes memory data = abi.encodePacked("some string", true, address(100), num, num1, byt);
-        return data;
-    }
-
-    function encodePackedData2(int num, int[] memory nums) public pure returns (bytes memory) {
-        bytes2[3] memory bytesData1 = [bytes2(0x1234), 0x5123, 0x3456];
-        bytes memory data = abi.encodePacked([1,2,3],[0x1, 0x111, 0x4543344555], num, nums, bytesData1);
-        return data;
     }
 
     // ---------- encoding string using bytes ----------- //
@@ -161,16 +120,6 @@ contract Encoding {
         return someString;
     }
 
-    function decodeString1(bytes memory encodedData) public pure returns (string memory) {
-        string memory someString = abi.decode(encodedData, (string));
-        return someString;
-    }
-
-    function decodeString2() public pure returns (string memory) {
-        string memory someString = abi.decode(encodeStrings2(), (string));
-        return someString;
-    }
-
     function decodeNumber() public pure returns (uint8) {
         uint8 someString = abi.decode(encodeNumber(), (uint8));
         return someString;
@@ -193,37 +142,46 @@ contract Encoding {
 
     // -------- packed decoding ------- //
     function decodePackedString() public pure returns (string memory) {
-        string memory someString = abi.decode(encodePackedString(), (string));
+        string memory someString = string(encodePackedString());
         return someString;
     }
 
-    function decodePackedString1() public pure returns (string memory) {
-        string memory someString = abi.decode(encodePackedString1(), (string));
+    function decodePackedNumber() public pure returns (uint256) {
+        uint256 someString = abi.decode(encodePackedNumber(), (uint256));
         return someString;
-    }
-
-    function decodePackedStrings2() public pure returns (string memory) {
-        string memory someString = abi.decode(encodePackedStrings2(), (string));
-        return someString;
-    }
-
-    function decodePackedAddress() public pure returns (address) {
-        address addr = abi.decode(encodePackedAddress(), (address));
-        return addr;
-    }
-
-    function decodePackedBoolean(bool val) public pure returns (bool) {
-        bool addr = abi.decode(encodePackedBoolean(val), (bool));
-        return addr;
     }
 
     function decodePackedByte() public pure returns (bytes4) {
-        bytes4 addr = abi.decode(encodePackedByte(), (bytes4));
+        bytes4 addr = bytes4(encodePackedByte());
         return addr;
     }
 
-
     // -------- multi encoding ------- //
+
+    function encodeString1() public pure returns (bytes memory) {
+        return abi.encode("Hi Mom! ", "Miss you!"); 
+    }
+
+    function encodeStrings2() public pure returns (bytes memory) {
+        return abi.encode("Hi Mom!    ", "  Miss       you!      "); 
+    }
+
+    function encodeData() public pure returns (bytes memory) {
+        bytes1 num = 0x23;
+        bytes4 num1 = 0x12345678;
+        bytes memory byt = "ZAID1234565433i33i4QWEE@@@$$##";
+        string memory someStr = "some string";
+        bytes memory data = abi.encode(50, someStr, true, address(100), num, num1, byt, "zaidi");
+        return data;
+    }
+
+    function encodeData2(int num, int[] memory nums) public pure returns (bytes memory) {
+        string[5] memory strData = ["1","2","3","4","5"];
+        bytes[3] memory bytesData = [bytes("0x12300041"), "0x546783453", "0x34567893"];
+        bytes2[3] memory bytesData1 = [bytes2(0x1234), 0x5123, 0x3456];
+        bytes memory data = abi.encode([1,2,3], ["1","2","3"],[0x1, 0x111, 0x4543344555], num, strData, nums, User(1, "name"), [User(1, "name"), User(2,"Name")], bytesData, bytesData1);
+        return data;
+    }
 
     function multiEncode() public pure returns (bytes memory) {
         return abi.encode("some string", "it's bigger!");
@@ -231,12 +189,49 @@ contract Encoding {
 
     // -------- multi decoding ------- //
 
+    function decodeString1() public pure returns (string memory, string memory) {
+        (string memory someString, string memory someOtherString) = abi.decode(encodeString1(), (string, string));
+        return (someString, someOtherString);
+    }
+
+    function decodeString2() public pure returns (string memory, string memory) {
+        (string memory someString, string memory someOtherString) = abi.decode(encodeStrings2(), (string, string));
+        return (someString, someOtherString);
+    }
+
+    function decodeData() public pure returns (uint8, string memory, bool, address, bytes1, bytes4, bytes memory, string memory) {
+        (uint8 a, string memory b, bool c, address d, bytes1 e, bytes4 f, bytes memory g, string memory h) = abi.decode(encodeData(), (uint8, string, bool, address, bytes1, bytes4, bytes, string));
+        return (a, b, c, d, e, f, g, h);
+    }
+
     function multiDecode() public pure returns (string memory, string memory) {
         (string memory someString, string memory someOtherString) = abi.decode(multiEncode(), (string, string));
         return (someString, someOtherString);
     }
 
     // -------- multi packed encoding ------- //
+
+    function encodePackedString1() public pure returns (bytes memory) {
+        return abi.encodePacked("Hi Mom! ", "Miss you!"); 
+    }
+
+    function encodePackedStrings2() public pure returns (bytes memory) {
+        return abi.encodePacked("Hi Mom!    ", "  Miss       you!      "); 
+    }
+
+    function encodePackedData() public pure returns (bytes memory) {
+        bytes1 num = 0x23;
+        bytes4 num1 = 0x12345678;
+        bytes memory byt = "ZAID1234565433i33i4QWEE@@@$$##";
+        bytes memory data = abi.encodePacked("some string", true, address(100), num, num1, byt);
+        return data;
+    }
+
+    function encodePackedData2(int num, int[] memory nums) public pure returns (bytes memory) {
+        bytes2[3] memory bytesData1 = [bytes2(0x1234), 0x5123, 0x3456];
+        bytes memory data = abi.encodePacked([1,2,3],[0x1, 0x111, 0x4543344555], num, nums, bytesData1);
+        return data;
+    }
     
     function multiEncodePacked() public pure returns (bytes memory){
         bytes memory someString = abi.encodePacked("some string", "it's bigger!");
@@ -244,6 +239,16 @@ contract Encoding {
     }
     
     // -------- multi packed decoding ------- //
+
+    function decodePackedString1() public pure returns (string memory) {
+        string memory someString = string(encodePackedString1());
+        return someString;
+    }
+
+    function decodePackedStrings2() public pure returns (string memory) {
+        string memory someString = string(encodePackedStrings2());
+        return someString;
+    }
 
     function multiStringCastPacked() public pure returns (string memory){ 
         string memory someString = string(multiEncodePacked());
